a5bdccbdf682bde5412d42719f5e486f77ab0300
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-03-27 12:03:16 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+// interface State {
+//   images: string[];
+// }
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
+export const App: React.FC = () => {
+
+  const [itemWidth, setItemWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+
+  const images = [
       './img/1.png',
       './img/2.png',
       './img/3.png',
@@ -19,21 +24,56 @@ class App extends React.Component<{}, State> {
       './img/8.png',
       './img/9.png',
       './img/10.png',
-    ],
-  };
-
-  render() {
-    const { images } = this.state;
+    ];
 
     return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <div className="app">
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
+        <form>
+          <label>
+            item width
+            <input
+              type="text"
+              value={itemWidth}
+              onChange={(event) => setItemWidth(+event.target.value)}
+            />
+          </label>
+          <label>
+            frame size
+            <input
+              type="text"
+              value={frameSize}
+              onChange={(event) => setFrameSize(+event.target.value)}
+            />
+          </label>
+          <label>
+            step
+            <input
+              type="text"
+              value={step}
+              onChange={(event) => setStep(+event.target.value)}
+            />
+          </label>
+          <label>
+            animation duration
+            <input
+              type="text"
+              value={animationDuration}
+              onChange={(event) => setAnimationDuration(+event.target.value)}
+            />
+          </label>
+        </form>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={false}
+        />
       </div>
     );
   }
-}
 
-export default App;
+// export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import './Carousel.scss';
+import React, { useState } from "react";
+import "./Carousel.scss";
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [next, setNext] = useState(0);
 
-export default Carousel;
+  return (
+    <div className="carousel">
+      <ul
+        className="carousel__list"
+        style={{ transform: `translateX(-${next}px)` }}
+      >
+        {images.map((image, index) => (
+          <li key={image}>
+            <img className="carousel__image" src={image} alt={index + 1 + ""} />
+          </li>
+        ))}
+      </ul>
+
+      <button type="button" className="button--prev">
+        Prev
+      </button>
+      <button
+        type="button"
+        className="button--next"
+        data-cy="next"
+        onClick={() => {
+          setNext((currentNext) => currentNext + itemWidth * step);
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+// export default Carousel;
